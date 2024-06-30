7f06332eb51ace56206599ccd79e0a65359d3d07
Daniel Baker <daniel.n.baker@gmail.com>
2024-06-29 19:40:22 -0700

    treesitter: add keybinds and update settings
    Added a keybind to toggle treesitter context.
    Turned off treesitter context by default and modified some settings.

--- config/treesitter.nix
+++ config/treesitter.nix
@@ -1,19 +1,75 @@
-{ lib, config, ... }:
+{
+  lib,
+  config,
+  nixvimLib,
+  ...
+}:
 let
   cfg = config.nixvimcfg.treesitter;
 in
 {
   options.nixvimcfg.treesitter.enable = lib.mkEnableOption "treesitter setup";
 
-  config = lib.mkIf cfg.enable {
+  config = lib.mkIf cfg.enable (
+    lib.mkMerge [
 
-    plugins.treesitter.enable = true;
-    plugins.treesitter-context.enable = true;
-    plugins.treesitter-refactor.enable = true;
-    #plugins.treesitter-refactor.highlightCurrentScope.enable = true;
-    plugins.treesitter-refactor.highlightDefinitions.enable = true;
-    plugins.treesitter-refactor.navigation.enable = true;
-    plugins.treesitter-refactor.smartRename.enable = true;
+      {
 
-  };
+        plugins.treesitter.enable = true;
+        plugins.treesitter-refactor.enable = true;
+        plugins.treesitter-refactor.highlightCurrentScope.enable = false;
+        plugins.treesitter-refactor.highlightDefinitions.enable = true;
+        plugins.treesitter-refactor.navigation.enable = true;
+        plugins.treesitter-refactor.smartRename.enable = true;
+
+      }
+
+      {
+
+        plugins.treesitter-context.enable = true;
+        plugins.treesitter-context.settings.enable = false;
+        plugins.treesitter-context.settings.trim_scope = "outer";
+
+      }
+
+      {
+
+        plugins.treesitter.folding = true;
+
+        extraConfigLua = ''
+          vim.opt.foldenable = false
+        '';
+
+      }
+
+      (lib.mkIf config.nixvimcfg.which-key.enable {
+
+        extraConfigLua =
+          let
+            mappings = {
+
+              t = {
+
+                name = "木 Treesitter";
+
+                c = [
+                  "<cmd>TSContextToggle<cr>"
+                  "Context Toggle"
+                ];
+              };
+            };
+            opts = {
+              prefix = "<leader>";
+            };
+          in
+          ''
+            require("which-key").register(
+              ${nixvimLib.helpers.toLuaObject mappings},
+              ${nixvimLib.helpers.toLuaObject opts}
+            )
+          '';
+
+      })
+    ]
+  );
 }
