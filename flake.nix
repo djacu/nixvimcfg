1999f171d2b1455b9d1b558c0988f6f161081646
Daniel Baker <daniel.n.baker@gmail.com>
2024-10-26 08:54:58 -0700

    neovim: overlay patch
    neovim 0.10.2 introduced a bug that causes an out of bounds error.
    The overlay fixes the bug via patch.

--- flake.nix
+++ flake.nix
@@ -36,6 +36,27 @@
           nvim = nixvim'.makeNixvimWithModule nixvimModule;
         in
         {
+          _module.args.pkgs = import inputs.nixpkgs {
+            inherit system;
+            overlays = [
+              (final: prev: {
+                neovim-unwrapped = prev.neovim-unwrapped.overrideAttrs (old: {
+                  patches = old.patches ++ [
+                    # Fix byte index encoding bounds.
+                    # - https://github.com/neovim/neovim/pull/30747
+                    # - https://github.com/nix-community/nixvim/issues/2390
+                    (final.fetchpatch {
+                      name = "fix-lsp-str_byteindex_enc-bounds-checking-30747.patch";
+                      url = "https://patch-diff.githubusercontent.com/raw/neovim/neovim/pull/30747.patch";
+                      hash = "sha256-2oNHUQozXKrHvKxt7R07T9YRIIx8W3gt8cVHLm2gYhg=";
+                    })
+                  ];
+                });
+              })
+            ];
+            config = { };
+          };
+
           checks = {
             # Run `nix flake check .` to verify that your config is not broken
             default = nixvimLib.check.mkTestDerivationFromNixvimModule nixvimModule;
