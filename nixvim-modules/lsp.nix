f38da07c43bc68007e8bb616c0b4160a095e5b4b
Daniel Baker <daniel.n.baker@gmail.com>
2025-10-18 14:53:11 -0700

    nixvim-modules.lsp: add white space for my eyes

--- nixvim-modules/lsp.nix
+++ nixvim-modules/lsp.nix
@@ -12,26 +12,43 @@ in
   config = lib.mkIf cfg.enable {
 
     plugins.lsp.enable = true;
+
     plugins.lsp.servers.astro.enable = true;
+
     plugins.lsp.servers.bashls.enable = true;
+
     plugins.lsp.servers.cssls.enable = true;
+
     plugins.lsp.servers.eslint.enable = true;
+
     plugins.lsp.servers.html.enable = true;
+
     plugins.lsp.servers.jsonls.enable = true;
+
     plugins.lsp.servers.nil_ls.enable = false;
+
     plugins.lsp.servers.nixd.enable = true;
+
     plugins.lsp.servers.ruff.enable = true;
+
     lsp.servers.rust_analyzer.enable = true;
     lsp.servers.rust_analyzer.packageFallback = true;
     plugins.lsp.servers.rust_analyzer.enable = true;
     plugins.lsp.servers.rust_analyzer.installCargo = true;
     plugins.lsp.servers.rust_analyzer.installRustc = true;
     plugins.lsp.servers.rust_analyzer.installRustfmt = true;
+
     plugins.lsp.servers.tailwindcss.enable = true;
+
     plugins.lsp.servers.taplo.enable = true;
+
     plugins.lsp.servers.texlab.enable = true;
+
     plugins.lsp.servers.typos_lsp.enable = true;
+
     plugins.lsp.servers.tinymist.enable = true;
+
     plugins.lsp.servers.yamlls.enable = true;
+
   };
 }
